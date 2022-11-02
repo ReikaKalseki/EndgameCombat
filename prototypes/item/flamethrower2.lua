1891163ec291c2f1f7c6b0b90d9eca99d5136154
Reika <reikasminecraft@gmail.com>
2022-11-02 18:40:45 -0400

    Update 116

--- prototypes/item/flamethrower2.lua
+++ prototypes/item/flamethrower2.lua
@@ -1,50 +1,7 @@
-data:extend(
-{
-  {
-    type = "gun",
-    name = "flamethrower-2",
-    icon = "__base__/graphics/icons/flamethrower.png",
-	icon_size = 32,
-    flags = {},
-    subgroup = "gun",
-    order = "e[flamethrower]",
-    attack_parameters =
-    {
-      type = "stream",
-      ammo_category = "flamethrower",
-      cooldown = 1,
-      movement_slow_down_factor = 0.6,
-      projectile_creation_distance = 0.6,
-      gun_barrel_length = 0.8,
-      gun_center_shift = { 0, -1 },
-      range = 30,
-      min_range = 1,
-      cyclic_sound =
-      {
-        begin_sound =
-        {
-          {
-            filename = "__base__/sound/fight/flamethrower-start.ogg",
-            volume = 0.7
-          }
-        },
-        middle_sound =
-        {
-          {
-            filename = "__base__/sound/fight/flamethrower-mid.ogg",
-            volume = 0.7
-          }
-        },
-        end_sound =
-        {
-          {
-            filename = "__base__/sound/fight/flamethrower-end.ogg",
-            volume = 0.7
-          }
-        }
-      }
-    },
-    stack_size = 5
-  }
-}
-)
+local flamethrower = table.deepcopy(data.raw.gun.flamethrower)
+flamethrower.name = "flamethrower-2"
+flamethrower.attack_parameters.range = 30
+
+data:extend({
+  flamethrower
+})
--- prototypes/overrides.lua
+++ prototypes/overrides.lua
@@ -243,8 +243,20 @@ if data.raw.car["heli-entity-_-"] then
 		if string.find(name, "heli", 1, true) then
 			--error(serpent.block(car.consumption .. " >> " .. string.sub(car.consumption, 1, -3) .. " >>> " .. tonumber(string.sub(car.consumption, 1, -3))))
 			if car.guns and #car.guns > 0 then --skip technical entities
+				table.remove(car.guns, 1) --remove the basic flamethrower
 				table.insert(car.guns, "flamethrower-2")
+				car.equipment_grid = "helicopter-equipment-grid"
 			end
 		end
 	end
+	
+	data:extend({
+	{
+		type = "equipment-grid",
+		name = "helicopter-equipment-grid",
+		width = 10,
+		height = 7,
+		equipment_categories = {"armor"}
+	}
+	})
 end
